85f4b9a30e5adae25231f66afa4871c5eb07af29
Jesús Aldea Jiménez <44441670+jaj712@users.noreply.github.com>
2019-04-11 17:11:11 +0200

    Set Element Model

---
+++ project/src/com/model/ElementoModel.java
@@ -0,0 +1,38 @@
+package com.model;
+
+public class ElementoModel {
+	
+	private int pk_elemento;
+	private String str_nombre_elemento;
+	
+	public ElementoModel() {
+		setPk_elemento(0);
+		setStr_nombre_elemento(null);
+	}
+
+	public ElementoModel(int pk_elemento, String str_nombre_elemento) {
+		
+		setPk_elemento(pk_elemento);
+		setStr_nombre_elemento(str_nombre_elemento);
+	}
+
+	public int getPk_elemento() {
+		return pk_elemento;
+	}
+	
+	public String getStr_nombre_elemento() {
+		return str_nombre_elemento;
+	}
+	
+	public void setStr_nombre_elemento(String str_nombre_elemento) {
+		this.str_nombre_elemento = str_nombre_elemento;
+	}
+	
+	public void setPk_elemento(int pk_elemento) {
+		this.pk_elemento = pk_elemento;
+	}
+	
+	
+	
+
+}
